cf58ab4d8e35b8a95bf3c587f4f35fe8b9e8a083
Imran Abdalla <nagatoyahiko856@gmail.com>
2021-03-27 05:39:50 -0400

    testing

--- frontUI/client/src/Routes.js
+++ frontUI/client/src/Routes.js
@@ -29,7 +29,7 @@ const Routes = () => {
     <Switch>
       <Route
         exact
-        path="/"
+        path="/home"
         render={matchProps => (
           <WithLayout
             {...matchProps}
