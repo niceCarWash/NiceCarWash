359f7ab902bdf912a317b35356a1888e2747d211
Nice Car Wash <info@nicecarwash.se>
2021-04-08 05:27:15 -0400

    make it btter

--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,8 @@ const { authCheck, adminCheck } = require('../middlewares/auth');
 const { getUsers } = require('../controllers/users');
 
 router.post('/users', authCheck, adminCheck, getUsers);
+router.get('/info', (req, res) => {
+  res.send('The server is runnig and you hit the info end point');
+});
 
 module.exports = router;
